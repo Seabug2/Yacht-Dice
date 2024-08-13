69d62de0ee5ffa07c0344b151c413cae731995da
정해창 <femgoon0425@gmail.com>
2024-08-13 16:39:13 +0900

    UI 기본 세팅

---
+++ Assets/YachtPlayer.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+public class YachtPlayer : NetworkBehaviour
+{
+    [SerializeField]
+    Text nickName;
+    [SerializeField]
+    Text rate;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+
+        }   
+    }
+
+    void Init()
+    {
+        //SQLManager의 info 정보에서 닉네임과 저적을 가져온다.
+        //nickName.text = 
+        //record.text = 
+    }
+}
