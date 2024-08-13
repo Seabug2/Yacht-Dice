0cd48ad0881351adb09375a73fbaf2482f446e0e
JihoonSung <168630251+Erlo282@users.noreply.github.com>
2024-08-13 16:41:28 +0900

    [Update] Add GameManager
    GameManager controls Score_Total & isGameOver

--- Assets/3.Script/GamePlay/GameManager.cs
+++ Assets/3.Script/GamePlay/GameManager.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private ScoreManager Score;
+    [SerializeField] private DiceController Dice;
+
+    public static int Score_Total;
+    public static bool isGameOver = false;
+
     private static GameManager instance = null;
     public static GameManager Instance
     {
@@ -19,11 +28,22 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if (instance == null)
+        if (Instance == null)
         {
             Destroy(gameObject);
         }
+
+        Score_Total = 0;
     }
 
+    public void AddScore(int score)
+    {
+        Score_Total += score;
+    }
 
+    public void GameOver()
+    {
+        isGameOver = true;
+        // If player 1 & player 2 are BOTH isGamveOver=true -----> Result UI & End game
+    }
 }
