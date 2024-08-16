16df68c10bf9b52ab683df35a8d3938bf157d6ba
JihoonSung <168630251+Erlo282@users.noreply.github.com>
2024-08-16 11:02:29 +0900

    [Update] Dice.cs

--- Assets/3.Script/GamePlay/PointSlot.cs
+++ Assets/3.Script/GamePlay/PointSlot.cs
@@ -4,5 +4,18 @@ using DG.Tweening;
 
 public class PointSlot : MonoBehaviour
 {
-   
+    public bool isSelected;
+    public int slot_currentScore;
+
+    [SerializeField] private Button slot_btn;
+    [SerializeField] private Text slot_txt;
+
+
+
+
+
+    virtual public int slotScore()
+    {
+        return slot_currentScore;
+    }
 }
